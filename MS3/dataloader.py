5e7c8332eb956c68cc62fe42488d09375bc3251f
WYS1998 <137267973+WangYuSenn@users.noreply.github.com>
2024-06-12 21:52:38 +0800

    Add files via upload

--- MS3/dataloader.py
+++ MS3/dataloader.py
@@ -1,4 +1,5 @@
 import os
+
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
@@ -11,7 +12,7 @@ import cv2
 from PIL import Image
 from torchvision import transforms
 
-from avs_ms3.config import cfg
+from config import cfg
 import pdb
 
 
@@ -27,16 +28,16 @@ def load_image_in_PIL_to_Tensor(path, mode='RGB', transform=None):
 def load_audio_lm(audio_lm_path):
     with open(audio_lm_path, 'rb') as fr:
         audio_log_mel = pickle.load(fr)
-    audio_log_mel = audio_log_mel.detach()# [5, 1, 96, 64]
+    audio_log_mel = audio_log_mel.detach() # [5, 1, 96, 64]
     return audio_log_mel
 
 
-class MS3Dataset(Dataset):
-    """Dataset for multiple sound source segmentation"""
+class S4Dataset(Dataset):
+    """Dataset for single sound source segmentation"""
     def __init__(self, split='train'):
-        super(MS3Dataset, self).__init__()
+        super(S4Dataset, self).__init__()
         self.split = split
-        self.mask_num = 5
+        self.mask_num = 1 if self.split == 'train' else 5
         df_all = pd.read_csv(cfg.DATA.ANNO_CSV, sep=',')
         self.df_split = df_all[df_all['split'] == split]
         print("{}/{} videos are used for {}".format(len(self.df_split), len(df_all), self.split))
@@ -49,26 +50,29 @@ class MS3Dataset(Dataset):
         ])
 
 
-
     def __getitem__(self, index):
         df_one_video = self.df_split.iloc[index]
-        video_name = df_one_video[0]
-        img_base_path =  os.path.join(cfg.DATA.DIR_IMG, video_name)
-        audio_lm_path = os.path.join(cfg.DATA.DIR_AUDIO_LOG_MEL, self.split, video_name + '.pkl')
-        mask_base_path = os.path.join(cfg.DATA.DIR_MASK, self.split, video_name)
+        video_name, category = df_one_video[0], df_one_video[2]
+        img_base_path =  os.path.join(cfg.DATA.DIR_IMG, self.split, category, video_name)
+        audio_lm_path = os.path.join(cfg.DATA.DIR_AUDIO_LOG_MEL, self.split, category, video_name + '.pkl')
+        mask_base_path = os.path.join(cfg.DATA.DIR_MASK, self.split, category, video_name)
         audio_log_mel = load_audio_lm(audio_lm_path)
         # audio_lm_tensor = torch.from_numpy(audio_log_mel)
         imgs, masks = [], []
         for img_id in range(1, 6):
-            img = load_image_in_PIL_to_Tensor(os.path.join(img_base_path, "%s.mp4_%d.png"%(video_name, img_id)), transform=self.img_transform)
+            img = load_image_in_PIL_to_Tensor(os.path.join(img_base_path, "%s_%d.png"%(video_name, img_id)), transform=self.img_transform)
             imgs.append(img)
         for mask_id in range(1, self.mask_num + 1):
-            mask = load_image_in_PIL_to_Tensor(os.path.join(mask_base_path, "%s_%d.png"%(video_name, mask_id)), transform=self.mask_transform, mode='P')
+            mask = load_image_in_PIL_to_Tensor(os.path.join(mask_base_path, "%s_%d.png"%(video_name, mask_id)), transform=self.mask_transform, mode='1')
             masks.append(mask)
         imgs_tensor = torch.stack(imgs, dim=0)
         masks_tensor = torch.stack(masks, dim=0)
 
-        return imgs_tensor, audio_log_mel, masks_tensor, video_name
+        if self.split == 'train':
+            return imgs_tensor, audio_log_mel, masks_tensor
+        else:
+            return imgs_tensor, audio_log_mel, masks_tensor, category, video_name
+
 
     def __len__(self):
         return len(self.df_split)
@@ -76,10 +80,8 @@ class MS3Dataset(Dataset):
 
 
 
-
-
 if __name__ == "__main__":
-    train_dataset = MS3Dataset('train')
+    train_dataset = S4Dataset('train')
     train_dataloader = torch.utils.data.DataLoader(train_dataset,
                                                      batch_size=2,
                                                      shuffle=False,
@@ -87,8 +89,8 @@ if __name__ == "__main__":
                                                      pin_memory=True)
 
     for n_iter, batch_data in enumerate(train_dataloader):
-        imgs, audio, mask, video_name = batch_data # [bs, 5, 3, 224, 224], [bs, 5, 1, 96, 64], [bs, 1, 1, 224, 224]
-        # imgs, audio, mask, video_name = batch_data # [bs, 5, 3, 224, 224], [bs, 5, 1, 96, 64], [bs, 1, 1, 224, 224]
+        imgs, audio, mask = batch_data # [bs, 5, 3, 224, 224], [bs, 5, 1, 96, 64], [bs, 1, 1, 224, 224]
+        # imgs, audio, mask, category, video_name = batch_data # [bs, 5, 3, 224, 224], [bs, 5, 1, 96, 64], [bs, 1, 1, 224, 224]
         pdb.set_trace()
     print('n_iter', n_iter)
     pdb.set_trace()
